d5ef39b198ddab9a7e518cce1633786f8b83b699
AdrianKauz <akauz@hispeed.ch>
2018-09-28 02:29:04 +0200

    - Finished Java-Task for "PCP-Übung Woche 1: Einstieg - C & Java revisited"

--- Java/ADTStack/src/Models/Element.java
+++ Java/ADTStack/src/Models/Element.java
@@ -4,20 +4,22 @@ import Interfaces.IElement;
 
 public class Element implements IElement
 {
+    private int value;
+    private Element nextElement;
 
     public Element(int value) {
-
+        this.value = value;
     }
 
     public int getValue() {
-        return 0;
+        return this.value;
     }
 
     public Element getNext() {
-        return null;
+        return this.nextElement;
     }
 
     public void setNext(Element next) {
-
+        this.nextElement = next;
     }
 }
--- Java/ADTStack/src/Models/Stack.java
+++ Java/ADTStack/src/Models/Stack.java
@@ -4,27 +4,66 @@ import Interfaces.IStack;
 
 public class Stack implements IStack
 {
-    public void push(Element e){
+    private static final int STACK_EMPTY_INDEX = -1;
+    private int index = -1;
+    private Element topElement;
 
+    public void push(Element e) {
+        if (this.topElement == null) {
+            this.topElement = e;
+        } else {
+            e.setNext(this.topElement);
+            this.topElement = e;
+        }
+
+        this.index++;
     }
 
-    public Element top(){
-        return null;
+    public Element top() {
+        return (this.index > Stack.STACK_EMPTY_INDEX) ? this.topElement : null;
     }
 
-    public boolean pop(){
-        return true;
+    public boolean pop() {
+        if (this.index > Stack.STACK_EMPTY_INDEX) {
+            this.topElement = this.topElement.getNext();
+            this.index--;
+
+            return true;
+        }
+
+        return false;
     }
 
-    public void print(){
+    public void print() {
+        if (this.index > Stack.STACK_EMPTY_INDEX) {
+            int topElement = this.topElement.getValue();
 
+            StringBuilder builder = new StringBuilder();
+            builder.append("print - Stack contains:");
+
+            Element currElement = this.topElement;
+            do {
+                builder.append(" ");
+                builder.append(Integer.toString(currElement.getValue()));
+                builder.append(",");
+
+                currElement = currElement.getNext();
+            } while (currElement != null);
+
+            builder.append(" top Element = ");
+            builder.append(Integer.toString(topElement));
+
+            System.out.println(builder.toString());
+        } else {
+            System.out.println("print - Stack is empty");
+        }
     }
 
-    public boolean isEmpty(){
-        return true;
+    public boolean isEmpty() {
+        return (this.index <= Stack.STACK_EMPTY_INDEX);
     }
 
-    public int size(){
-        return 0;
+    public int size() {
+        return this.index + 1;
     }
 }
